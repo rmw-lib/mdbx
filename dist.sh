c108b251b75aa8fa1ac13451ef353ee2263791b6
gcxfd <i@rmw.link>
2021-11-24 18:24:41 +0800

    🔶

--- dist.sh
+++ dist.sh
@@ -15,8 +15,11 @@ npx @rmw/md-include .markdown.json
 
 cargo set-version --bump patch
 
+version=$(cat Cargo.toml|grep "^version"|awk -F\" '{print $2}')
+
 git add -u
-git commit -m dist
+git commit -m "v$version"
+git tag v$version
 git push
 
 cargo +nightly publish
