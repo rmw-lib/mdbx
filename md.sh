2f57d549fbafce63640a96425623f9f919ce58e1
gcxfd <i@rmw.link>
2021-12-13 15:09:46 +0800

    🔶

--- md.sh
+++ md.sh
@@ -4,6 +4,8 @@ DIR=$(cd "$(dirname "$0")"; pwd)
 set -ex
 cd $DIR
 
+npx md-padding ./readme.make.md
+
 npx @rmw/md-include .markdown.json
 
 cp README.md ../blog-vuepress2/site/zh/log/2021-12-21-mdbx.md
