e10bbe125d46c3c5f4a8a6212739137e5322bf6e
gcxfd <i@rmw.link>
2021-12-16 04:48:09 +0000

    .

--- mdbx-proc/dist.sh
+++ mdbx-proc/dist.sh
@@ -17,7 +17,7 @@ version=v$(cat Cargo.toml|grep "^version"|awk -F\" '{print $2}')
 git add -u
 git commit -m mdbx-proc-$version
 git tag mdbx-proc-$version
-git push mdbx-proc-$version
+git push mdbx-proc-$version -f
 
 cargo +nightly publish
 
